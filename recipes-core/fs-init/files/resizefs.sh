bf6a064c5b7c78db278154c500bf865816abc59f
rikardo <rikard.olander@hostmobility.com>
2022-10-24 14:26:08 +0000

    Resizefs rootfs Mx-V: T30: Update resize script and service
    Use fdisk and resize2fs to full size. if fdisk
    table does not update than restart system and try again.

--- recipes-core/fs-init/files/resizefs.sh
+++ recipes-core/fs-init/files/resizefs.sh
@@ -9,54 +9,33 @@ if [ -z $1 ]; then
 fi
 
 if [ -z $2 ]; then
-    echo "Missing in parameter to target PART (mmcblkXpX)"
+    echo "Missing in parameter to target PART ($1pX)"
     exit 1
 fi
 
 DISK="$1"
 PART="$2"
 
-logger "resizing $PART to fill its full partition size"
+logger "resizing ${DISK}p$PART to fill its full partition size"
 # get the disk total size
-DISK_SIZE=`cat /sys/block/$DISK/size`
+#DISK_SIZE=`cat /sys/block/$DISK/size`
 
 # get partition start and size
-PART_OFF=`cat /sys/block/$DISK/$PART/start`
-PART_SIZE=`cat /sys/block/$DISK/$PART/size`
+PART_OFF=`cat /sys/block/$DISK/${DISK}p$PART/start`
+#PART_SIZE=`cat /sys/block/$DISK/${DISK}p$PART/size`
 
-# calculate size after the partition to the end of disk
-SPARE=`expr $DISK_SIZE - $PART_OFF - $PART_SIZE`
+# Resize now
+printf "d\n$PART\nn\np\n$PART_OFF\n\nw\n" | fdisk -B  /dev/mmcblk0
+resize2fs /dev/${DISK}p$PART
 
-# new filesystem size, it must not overlap the secondary gpt header
-# assume 1024kB as GPT size (is 34 sectors)
-if [ $SPARE -lt 2048 ]
-then
-	FSSIZE=`expr $PART_SIZE - 2048`
-else
-	FSSIZE=$PART_SIZE
+#Check size or reboot might be needed to get full resize.
+DISK_SPACE_AVAILABLE=$(df --output=avail "$PWD" | sed '1d;s/[^0-9]//g')
+if [[ $DISK_SPACE_AVAILABLE -le 1000000 ]]; then
+	echo "Warning: to litle space avaible: $(df --output=avail -h ),\n rebooting system..." systemd-cat -p emerg
+	reboot
+	exit 1
 fi
-
-# resize now
-
-#reduce I/O load by doing this in STEPSIZE increments
-#idea stolen from here:
-#https://codereview.chromium.org/551127
-
-#start from the current size in blocks of 512 byte, add STEPSIZE on each iteration
-STEPSIZE=`expr 64 \* 1024 \* 2`
-NEXTSIZE=`df /dev/$PART | grep /dev/root | awk '{print $2}'`
-NEXTSIZE=`expr $NEXTSIZE \* 2`
-NEXTSIZE=`expr $NEXTSIZE + $STEPSIZE`
-while [ $NEXTSIZE -lt $FSSIZE ]; do
-	FSSIZEMEG=`expr $NEXTSIZE / 2 / 1024`"M"
-	resize2fs /dev/$PART $FSSIZEMEG
-	sleep 1
-	NEXTSIZE=`expr $NEXTSIZE + $STEPSIZE`
-done
-FSSIZEMEG=`expr $FSSIZE / 2 / 1024`"M"
-resize2fs /dev/$PART $FSSIZEMEG
-
 #job done, remove it from systemd services
 systemctl --no-reload disable resizefs.service
 
-logger "resizing $PART finished, new size is $FSSIZEMEG"
+logger "resizing ${DISK}p$PART finished, new size is $(df --output=avail -h )"
