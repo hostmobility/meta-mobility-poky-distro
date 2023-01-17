1459416d0c0fcf2bef54f2669c1d82527488b785
rikardo <rikard.olander@hostmobility.com>
2023-01-17 13:35:15 +0000

    Host Insight: Add helper that installs fallback identity
    Add a helper that fetches the serial number from the device (mxv or
    mx4-c61) and writes it along with a domain to the insight client's
    configuration directory.

---
+++ recipes-support/host-insight-helper/host-insight-helper/host-insight-helper.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ID_PATH="/etc/opt/host-insight-client/identity-fallback.toml"
+
+# Get the serial number of the device.
+if [ "$(cat /etc/platform-system-type)" = "c61" ]; then
+  # Sleep until we know that pic_upgrade is complete
+  sleep 30
+  echo "uid = \"$(cat /opt/hm/pic_attributes/ctrl_serial_nr)\"" > $ID_PATH
+else
+  echo "uid = \"$(cat /proc/device-tree/chosen/serial-number)\"" > $ID_PATH
+fi
+
+# Set fallback domain
+echo "domain = \"deploy.hostmobility.org\"" >> $ID_PATH
+
+systemctl disable host-insight-helper
