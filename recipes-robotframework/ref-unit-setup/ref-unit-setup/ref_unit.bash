7caf7dbb2cd609b4c71f7e7e45edce483a47c1f2
rikardo <rikard.olander@hostmobility.com>
2023-11-17 12:13:33 +0000

    HMX:PRODUCTION: Fix ref_unit setup and service fix missing folder fix so setup can is on a prestart and gui is on start. if the gui has not started can is proberly the cause of it. shutdown DUT and restart ref

--- recipes-robotframework/ref-unit-setup/ref-unit-setup/ref_unit.bash
+++ recipes-robotframework/ref-unit-setup/ref-unit-setup/ref_unit.bash
@@ -2,11 +2,9 @@
 
 # Copyright (c) 2023 Host Mobility
 
-#Start Hostmobility Production tester Gui
+#Start Hostmobility Production tester prestart Setup.
 
 echo "Setup CAN channels so they will not lock us out with sleeping trancivers."
 for ((i=0;i<6;i++));do ip link set can$i down; ip link set can$i up type can bitrate 500000 dbitrate 4000000 fd on restart-ms 1000; done
 
-#Enable GUI
-python3 HostMobilityProductionTestGUI/gui.py
 
