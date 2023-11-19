e6f80d271e5fc72b186ac758e9141cc05a75eee0
rikardo <rikard.olander@hostmobility.com>
2023-11-19 16:13:09 +0100

    HMX:REF:_PRODUCTION: fix hosts ip names and set can up from start

--- recipes-robotframework/ref-unit-setup/ref-unit-setup/ref_unit_setup.bash
+++ recipes-robotframework/ref-unit-setup/ref-unit-setup/ref_unit_setup.bash
@@ -13,6 +13,13 @@ mv -f /lib/systemd/network/81-eth1.network /lib/systemd/network/80-eth1.network
 mv -f /lib/systemd/network/81-eth2.network /lib/systemd/network/80-eth2.network
 mv -f /lib/systemd/network/81-eth3.network /lib/systemd/network/80-eth3.network
 
+rm /lib/systemd/network/80-can0.network
+rm /lib/systemd/network/80-can1.network
+rm /lib/systemd/network/80-can2.network
+rm /lib/systemd/network/80-can3.network
+rm /lib/systemd/network/80-can4.network
+rm /lib/systemd/network/80-can5.network
+
 
 #install tools that is not in poky when we have internet access.
 while true; do
@@ -44,6 +51,25 @@ hostname="REF-HMX-${serial_number}"
 # Update the hostname
 echo "$hostname" > /etc/hostname
 hostname "$hostname"
+
+#Configure etc/hosts
+cat << EOF >> /etc/hosts
+192.168.1.200 dut_eth0
+192.168.1.201 ref_eth0
+192.168.1.20 mxv_eth0
+
+192.168.2.200 dut_eth1_default
+192.168.2.201 ref_eth1_default
+
+192.168.3.200 dut_eth2
+192.168.4.201 ref_eth2
+
+192.168.4.200 dut_eth3
+192.168.3.201 ref_eth3
+EOF
+
+
+
 # configuration is done stop this service and disable it we dont need it any more.
 systemctl disable ref_unit_setup.service
 systemctl stop ref_unit_setup.service
