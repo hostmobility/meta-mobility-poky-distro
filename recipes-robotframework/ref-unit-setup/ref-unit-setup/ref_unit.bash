9f225094838faa415d795188a52cfae0a28ce6e8
rikardo <rikard.olander@hostmobility.com>
2023-11-19 16:13:09 +0100

    Robot: Add Ref_unit.bash setup can and start gui

--- recipes-robotframework/ref-unit-setup/ref-unit-setup/ref_unit.bash
+++ recipes-robotframework/ref-unit-setup/ref-unit-setup/ref_unit.bash
@@ -4,7 +4,7 @@
 
 #Start Hostmobility Production tester Gui
 
-#TODO.
-echo "TODO add start path"
-sleep 1
+echo "Setup CAN channels so they will not lock us out with sleeping trancivers."
+for ((i=0;i<6;i++));do ip link set can$i down; ip link set can$i up type can bitrate 500000 dbitrate 4000000 fd on restart-ms 1000; done
+python3 HostMobilityProductionTestGUI/gui.py
 
