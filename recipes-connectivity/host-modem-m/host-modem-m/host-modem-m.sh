e059b1b59e93704d3fbd7b9d2b35dbcbb092dfb2
Albin Söderqvist <albin@albinutils.se>
2024-06-10 16:42:59 +0200

    host-modem-m: Split host-modem-m.sh into platform-specific variants
    - Assume that the platform and modem are known
    - Add some checks
    - Remove bashisms
    - Update bb-recipe to install the platform-specific script
    
    Co-authored-by: Rikard Olander <rikard.olander@hostmobility.com>

--- recipes-connectivity/host-modem-m/host-modem-m/host-modem-m.sh
+++ recipes-connectivity/host-modem-m/host-modem-m/host-modem-m.sh
@@ -1,74 +1,9 @@
-#!/usr/bin/env bash
-function modem_start()
-{
-    # Is the machine MX-V?
-    if gpiofind MODEM_ENABLE_ON; then
-        if gpioset $(gpiofind MODEM_ENABLE_ON)=1; then
-        echo "FAILED gpioset (gpiofind MODEM_ENABLE_ON)!"
-        fi
-    ## If we need to control two sim in this test in th future sim1 is controlled by a 0 and sim2 by 1.
-    ##  gpioset $(gpiofind SIM_SEL)=0
-    ##  if [ $? != 0 ]; then
-    ##echo "FAILED gpioset (gpiofind SIM_SEL!" >> $SETUP_LOG
-    ##fi
-    else
-    # Assume MX-4
-        WAIT_TIME=0
-        WAIT_EXIT=1
-        logger "check co cpu modem status"
-        until [ $WAIT_EXIT -eq 0 ] || [ $WAIT_TIME -eq 5 ]; do
-        echo 1 > /opt/hm/pic_attributes/ctrl_modem_on
-        sleep 6
-        CTRL_STATE=$(/opt/hm/modem_status.sh | grep "Ctrl state" | awk '{print $3}')
-        if [[ "$CTRL_STATE" == "ON" ]]; then
-            logger "modem started"
-            WAIT_EXIT=0
-        fi
-        WAIT_TIME=$((WAIT_TIME+1))
+#!/bin/sh
 
-        done
-    fi
-        sleep 1
+# Generic Host modem manager for platforms with modem on
 
-    WAIT_TIME=0
-    WAIT_EXIT=1
-    WAIT_TIME_OUT=60
-    logger "Determining modem type..."
-    until [ $WAIT_EXIT -eq 0 ] || [ $WAIT_TIME -eq $WAIT_TIME_OUT ]; do
-    if [ -e /dev/ttyUSB2 ]; then 
-        logger "EG25-G modem detected"
-        WAIT_EXIT=0
-    fi
-    WAIT_TIME=$((WAIT_TIME+1))
-    sleep 1
-    done
+WVDIAL_CONF=/opt/host-modem-m/onomondo-wvdial.conf
 
-    if [ $WAIT_TIME -eq $WAIT_TIME_OUT ]; then
-    logger "FAILED to start Modem, try again!"
-    return 1
-    fi
+wvdial -C $WVDIAL_CONF
+exit $?
 
-    return 0
-}
-
-function modem_stop()
-{
-    if gpiofind MODEM_ENABLE_ON; then
-        if gpioset $(gpiofind MODEM_ENABLE_ON)=0; then
-        echo "FAILED gpioset (gpiofind MODEM_ENABLE_ON)!"
-        fi
-    else
-        echo 0 > /opt/hm/pic_attributes/ctrl_modem_on
-    fi
-}
-
-
-modem_start
-RET=$?
-if (( RET == 0 )); then
-    wvdial -C /opt/host-insight-m/onomondo-wvdial.conf
-    exit $?
-else
-    modem_stop
-    exit 1
-fi
---
+++ recipes-connectivity/host-modem-m/host-modem-m/tegra/host-modem-m.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# Host modem manager for MX-4 T30 with PLS-8
+
+WVDIAL_CONF=/opt/host-modem-m/onomondo-wvdial.conf
+
+CTRL_MODEM_ON=/opt/hm/pic_attributes/ctrl_modem_on
+if [ -z "$CTRL_MODEM_ON" ]; then
+  logger "Failed to find $CTRL_MODEM_ON"
+  exit 1
+fi
+
+modem_start()
+{
+  WAIT_TIME=0
+  WAIT_EXIT=1
+  logger "check co cpu modem status"
+  until [ $WAIT_EXIT -eq 0 ] || [ $WAIT_TIME -eq 5 ]; do
+    echo 1 > $CTRL_MODEM_ON
+    sleep 6
+    CTRL_STATE=$(/opt/hm/modem_status.sh | grep "Ctrl state" | awk '{print $3}')
+    if [ "$CTRL_STATE" = "ON" ]; then
+      logger "modem started"
+      WAIT_EXIT=0
+    fi
+    WAIT_TIME=$((WAIT_TIME+1))
+
+  done
+  sleep 1
+
+  WAIT_TIME=0
+  WAIT_EXIT=1
+  WAIT_TIME_OUT=60
+  logger "Determining modem type..."
+  until [ $WAIT_EXIT -eq 0 ] || [ $WAIT_TIME -eq $WAIT_TIME_OUT ]; do
+    if [ -e /dev/ttyACM2 ]; then
+      logger "PLS-8 modem detected"
+      WAIT_EXIT=0
+    fi
+    WAIT_TIME=$((WAIT_TIME+1))
+    sleep 1
+  done
+
+  if [ $WAIT_TIME -eq $WAIT_TIME_OUT ]; then
+    logger "FAILED to start Modem, try again!"
+    return 1
+  fi
+
+  return 0
+}
+
+modem_stop()
+{
+  echo 0 > $CTRL_MODEM_ON
+}
+
+modem_start
+RET=$?
+if [ $RET = 0 ]; then
+  wvdial -C $WVDIAL_CONF
+  exit $?
+else
+  modem_stop
+  exit 1
+fi
