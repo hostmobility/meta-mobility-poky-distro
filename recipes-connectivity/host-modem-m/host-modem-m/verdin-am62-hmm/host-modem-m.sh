3b75576d95cb44a9245f2f110c1f05b9dc0cd2ef
kongstrupp <wilhelm.hacht@setek.se>
2025-07-02 15:40:21 +0000

    host-modem-m: Add support for auto modem bring up

---
+++ recipes-connectivity/host-modem-m/host-modem-m/verdin-am62-hmm/host-modem-m.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# Host modem manager for Host Monitor Mini
+
+CUSTOM_CONF=/opt/host-modem-m/wvdial.conf
+CUSTOM_SECTION="[Dialer Custom]"
+CUSTOM_DIALER="Custom"
+
+MODEM_GPIO=$(gpiofind MODEM_ENABLE_ON)
+if [ -z "$MODEM_GPIO" ]; then
+  logger "Failed to find GPIO for MODEM_ENABLE_ON"
+  exit 1
+fi
+
+modem_start()
+{
+  gpioset $MODEM_GPIO=1
+
+  sleep 1
+
+  WAIT_TIME=0
+  WAIT_EXIT=1
+  WAIT_TIME_OUT=60
+  logger "Waiting for modem..."
+  until [ $WAIT_EXIT -eq 0 ] || [ $WAIT_TIME -eq $WAIT_TIME_OUT ]; do
+    if [ -e /dev/ttyUSB2 ]; then
+      logger "Modem detected"
+      WAIT_EXIT=0
+    fi
+    WAIT_TIME=$((WAIT_TIME+1))
+    sleep 1
+  done
+
+  if [ $WAIT_TIME -eq $WAIT_TIME_OUT ]; then
+    logger "FAILED to start Modem, try again!"
+    return 1
+  fi
+
+  return 0
+}
+
+modem_stop()
+{
+  gpioset $MODEM_GPIO=0
+}
+
+modem_start
+RET=$?
+if [ $RET = 0 ]; then
+  if [ -f "$CUSTOM_CONF" ]; then
+    if grep -qF "$CUSTOM_SECTION" "$CUSTOM_CONF"; then
+      wvdial -C "$CUSTOM_CONF" "$CUSTOM_DIALER"
+    else
+      wvdial -C "$CUSTOM_CONF"
+    fi
+  else
+    wvdial
+  fi
+  exit $?
+else
+  modem_stop
+  exit 1
+fi
